63754348c7d0d6464ea322becc8eabef9175b3fa
Juan P. Isaza <jisazamp@gmail.com>
2022-05-31 21:44:55 -0500

    Added the basic components (Header, Form, Patients)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import Patients from './components/Patients';
 
 const App = () => {
-  return <div>Hola, mundo</div>;
+  return (
+    <>
+      <Header />
+      <section className='main-content'>
+        <Form />
+        <Patients />
+      </section>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <h1 className='section-header'>
+      Seguimiento pacientes <span className='highlighted'>Veterinaria</span>
+    </h1>
+  );
+};
+
+export default Header;
