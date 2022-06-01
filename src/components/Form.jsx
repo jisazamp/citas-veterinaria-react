25403e724a7a4fc3fa35c13d13ec26bb312f6db1
Juan P. Isaza <jisazamp@gmail.com>
2022-05-31 22:37:57 -0500

    Added and styled the rest of the input fields

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,6 +18,59 @@ const Form = () => {
             placeholder='Siara'
           />
         </div>
+
+        <div className='pets-form__input-group'>
+          <label htmlFor='ownerName' className='pets-form__input-label'>
+            Nombre del propietario
+          </label>
+          <input
+            id='ownerName'
+            className='pets-form__input-field'
+            type='text'
+            placeholder='Juan Pablo Isaza'
+          />
+        </div>
+
+        <div className='pets-form__input-group'>
+          <label htmlFor='ownerEmail' className='pets-form__input-label'>
+            E-mail del propietario
+          </label>
+          <input
+            id='ownerEmail'
+            className='pets-form__input-field'
+            type='email'
+            placeholder='juan@mail.com'
+          />
+        </div>
+
+        <div className='pets-form__input-group'>
+          <label htmlFor='medicalRelease' className='pets-form__input-label'>
+            Alta
+          </label>
+          <input
+            id='medicalRelease'
+            className='pets-form__input-field'
+            type='date'
+          />
+        </div>
+
+        <div className='pets-form__input-group'>
+          <label htmlFor='observations' className='pets-form__input-label'>
+            Síntomas
+          </label>
+          <input
+            id='observations'
+            className='pets-form__input-field'
+            type='textarea'
+            placeholder='Vómito desde hace 2 días.'
+          />
+        </div>
+
+        <div className='pets-form__input-group'>
+          <button className='btn btn-primary' type='submit'>
+            Agregar paciente
+          </button>
+        </div>
       </form>
     </div>
   );
